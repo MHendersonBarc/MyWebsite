c499d6816c7dd0c3be8214c94a267491e636814c
MHendersonBarc <michael.henderson@barclays.com>
2024-05-03 16:10:43 +0100

    Random message generator

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const navBar = document.querySelector('#navBar');
 const headAndNav = document.querySelector('#headAndNav');
 let missCounter = 0;
 
+function randomMessage(){
+    let randomNum = Math.floor(Math.random() * 5);
+    if (randomNum == 0){
+        return 'Try Harder!';
+    }
+    else if (randomNum == 1){
+        return 'Ooooo so close...';
+    }
+    else if (randomNum == 2){
+        return 'Now that one was terrible.';
+    }
+    else if (randomNum == 3){
+        return 'Are you even trying?';
+    }
+    else if (randomNum == 4){
+        return 'I BELIEVE IN YOU';
+    }
+}
+
 headAndNav.addEventListener('click', function(){
     missCounter ++;
     document.getElementById('missCounter').innerHTML = 'You have missed the buttons: ' + missCounter + ' times';
+    document.getElementById('randomMessage').innerHTML = randomMessage();
 })
